9ea803c225c5dae0b44d99d79115d949fc80433d
martin2000002 <martin200002@hotmail.com>
2025-04-21 01:57:01 -0500

    Commit para implementar el header funcionando

--- src/ui/header.asm
+++ src/ui/header.asm
@@ -1,4 +1,3 @@
-; header.asm - Implementación del encabezado de la interfaz para Buscaminas 3D
 .386
 .model flat, stdcall
 option casemap:none
@@ -23,15 +22,17 @@ EXTERN gameState:GameState
 
 .data
 ; Color para el área de interfaz superior
-UI_BRUSH_COLOR  EQU 00F0F0F0h  ; Color gris claro
+UI_BRUSH_COLOR  EQU 00574241h
 
 ; Strings para mostrar
 FlagCountStr    db "10", 0
 TimerStr        db "00:00", 0
 
-formatStr db "topAreaHeight=%d, iconSize=%d, centerY=%d", 0
-iconPosStr db "Posición del icono: xPos=%d, centerY=%d", 0
-buffer db 256 dup(0)  
+PercentageGroupSpace   DWORD 20
+IconTextSpace   DWORD 10
+
+.data?
+GroupSpace       DWORD ?
 
 .code
 
@@ -43,7 +44,8 @@ DrawHeader proc hWnd:HWND, hdc:HDC
     LOCAL hClockIcon:HICON
     LOCAL hInstance:HINSTANCE
     LOCAL topAreaHeight:DWORD
-    LOCAL centerY:DWORD
+    LOCAL centerYIcons:DWORD
+    LOCAL centerYTexts:DWORD
     LOCAL timerTextWidth:DWORD
     LOCAL textWidth:DWORD
     LOCAL textHeight:DWORD
@@ -53,7 +55,9 @@ DrawHeader proc hWnd:HWND, hdc:HDC
     LOCAL newFont:HFONT
     LOCAL oldBkMode:DWORD
     
-    ; Crear un rectángulo para el área de interfaz superior (10% del alto de la ventana)
+    ;-------------------------------------------------------------
+    ; BACKGROUND DEL HEADER
+    ;-------------------------------------------------------------
     invoke GetClientRect, hWnd, addr rect
     mov eax, rect.bottom
     mov ebx, 10              ; Dividir por 10 para obtener el 10%
@@ -71,28 +75,36 @@ DrawHeader proc hWnd:HWND, hdc:HDC
     
     ; Liberar el pincel
     invoke DeleteObject, hBrush
+
+    ;-------------------------------------------------------------
+    ; MEDIDAS PARA ALINEACIONES
+    ;-------------------------------------------------------------
+    ; Anchos de textos
+    invoke GetTextExtentPoint32, hdc, ADDR FlagCountStr, 2, ADDR textWidth
+
+    invoke GetTextExtentPoint32, hdc, ADDR TimerStr, 5, ADDR timerTextWidth
     
-    ; Obtener ancho del área cliente
-    invoke GetClientRect, hWnd, addr rect
-    
-    ; Ajustar tamaño de icono al 80% de la altura del área superior
+
+    ; Anchos/Largos de iconos
     mov eax, topAreaHeight
     mov ebx, 8          ; Multiplicar por 0.8 (8/10)
     mul ebx
     mov ebx, 10
     div ebx              ; eax = topAreaHeight * 0.8
     mov iconSize, eax
-    
-    ; Calcular posición vertical centrada para los íconos y texto
-    mov eax, topAreaHeight
-    sub eax, iconSize
-    shr eax, 1          ; Dividir por 2
-    mov centerY, eax    ; centerY es la posición Y para los íconos
 
-    ; ----- DEPURACIÓN: Mostrar valores en la ventana de Debug -----
-    invoke wsprintf, ADDR buffer, ADDR formatStr, topAreaHeight, iconSize, centerY
-    invoke OutputDebugString, ADDR buffer
+    ; Espacio entre grupos
+    mov eax, rect.right
+    mov ebx, PercentageGroupSpace
+    mul ebx
+    mov ebx, 100
+    div ebx
+    mov GroupSpace, eax
     
+    ;-------------------------------------------------------------
+    ; CONFIGURACION DE FUENTE
+    ;-------------------------------------------------------------
+
     ; Establecer modo de fondo transparente para el texto
     invoke SetBkMode, hdc, TRANSPARENT
     mov oldBkMode, eax
@@ -104,8 +116,9 @@ DrawHeader proc hWnd:HWND, hdc:HDC
     mul ebx
     mov ebx, 10
     div ebx              ; eax = iconSize * 0.7 (70% del tamaño del icono)
+    mov textHeight, eax
     
-    invoke CreateFont, eax, 0, 0, 0, FW_BOLD, 0, 0, 0, \
+    invoke CreateFont, textHeight, 0, 0, 0, FW_BOLD, 0, 0, 0, \
                       DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, \
                       CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, \
                       DEFAULT_PITCH or FF_DONTCARE, NULL
@@ -114,106 +127,101 @@ DrawHeader proc hWnd:HWND, hdc:HDC
     ; Seleccionar la nueva fuente en el contexto de dispositivo
     invoke SelectObject, hdc, newFont
     mov oldFont, eax
-    
-    ; Obtener la instancia actual
-    invoke GetWindowLong, hWnd, GWL_HINSTANCE
-    mov hInstance, eax
-    
-    ; ---------- Posicionar elementos en el centro horizontal ----------
-    ; Calcular el ancho total que ocuparán todos los elementos
-    ; Supongamos: [FLAG][espacio10][10][espacio50][CLOCK][espacio10][00:00]
 
-    ; Obtener ancho del texto del contador (FlagCountStr)
-    invoke GetTextExtentPoint32, hdc, addr FlagCountStr, 2, addr textWidth
+    ;-------------------------------------------------------------
+    ; CALCULO DE ALINEACIONES VERTICALES
+    ;-------------------------------------------------------------
 
-    ; Obtener ancho del texto del timer (TimerStr)
-    invoke GetTextExtentPoint32, hdc, addr TimerStr, 5, addr timerTextWidth
+    ; Calcular posición vertical para centrar el texto con el icono
+    mov eax, topAreaHeight
+    sub eax, textHeight
+    shr eax, 1           ; Dividir por 2 para centrar
+    mov centerYTexts, eax
+
+    ; Posicion centrada de y para iconos
+    mov eax, topAreaHeight
+    sub eax, iconSize
+    shr eax, 1          ; Dividir por 2
+    mov centerYIcons, eax    ; centerYIcons es la posición Y para los íconos
     
-    ; Obtener ancho del área cliente
-    invoke GetClientRect, hWnd, addr rect
+
+    ;-------------------------------------------------------------
+    ; CALCULO DE ALINEACION HORIZONTAL
+    ;-------------------------------------------------------------
     
     ; Calcular ancho total de cada grupo:
-    ; Grupo bandera: iconSize + 10 (espacio) + textWidth
-    ; Grupo reloj: iconSize + 10 (espacio) + timerTextWidth
+    ; Grupo flag: iconSize + 10 (espacio) + textWidth
     mov eax, iconSize
-    add eax, 10
+    add eax, IconTextSpace
     add eax, textWidth
+
+    ; Grupo clock: iconSize + 10 (espacio) + timerTextWidth
     mov ebx, iconSize
-    add ebx, 10
+    add ebx, IconTextSpace
     add ebx, timerTextWidth
 
     ; Espacio entre los dos grupos (150 píxeles)
-    mov ecx, 150
+    mov ecx, GroupSpace
 
-    ; Ancho total de todos los elementos: eax (grupo bandera) + ecx (espacio) + ebx (grupo reloj)
+    ; Ancho total de ambos grupos
     add eax, ecx
     add eax, ebx
 
     ; Calcular punto de inicio (centro - ancho_total/2)
     mov edx, rect.right
-    shr edx, 1          ; Centro de la ventana (rect.right / 2)
     sub edx, eax
     shr edx, 1          ; edx = centro - ancho_total/2 (para centrar todo)
 
     ; Posición inicial para el icono de bandera
     mov xPos, edx
+
+    ;-------------------------------------------------------------
+    ; MOSTRAR GRUPO FLAG
+    ;-------------------------------------------------------------
+
+    ; Obtener la instancia actual
+    invoke GetWindowLong, hWnd, GWL_HINSTANCE
+    mov hInstance, eax
     
-    ; ---------- Mostrar ícono de bandera y contador ----------
     ; Cargar el ícono de bandera
     invoke LoadImage, hInstance, IDI_FLAG, IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR
     mov hFlagIcon, eax
     
     ; Dibujar el ícono de bandera
-    invoke DrawIconEx, hdc, xPos, centerY, hFlagIcon, iconSize, iconSize, 0, NULL, DI_NORMAL
-
-    ; ----- DEPURACIÓN: Mostrar posición del icono -----
-    invoke wsprintf, ADDR buffer, ADDR iconPosStr, xPos, centerY
-    invoke OutputDebugString, ADDR buffer
+    invoke DrawIconEx, hdc, xPos, centerYIcons, hFlagIcon, iconSize, iconSize, 0, NULL, DI_NORMAL
     
-    ; Obtener dimensiones del texto del contador para centrarlo verticalmente
-    invoke GetTextExtentPoint32, hdc, addr FlagCountStr, 2, addr textWidth
-    mov eax, textWidth
-    mov textWidth, eax
-    
-    ; Calcular posición vertical para centrar el texto con el icono
-    mov eax, topAreaHeight
-    sub eax, textHeight
-    shr eax, 1           ; Dividir por 2 para centrar
-    
-    ; Dibujar el contador de banderas (alineado verticalmente con el icono)
-    mov eax, iconSize
-    add xPos, eax        ; Usar registro para sumar iconSize
-    add xPos, 10         ; Pequeño espacio entre icono y texto
-    invoke TextOut, hdc, xPos, centerY, addr FlagCountStr, 2
-    
-    ; ---------- Mostrar ícono de reloj y timer ----------
-    ; Posición para el reloj: centro + espacio/2
-    mov eax, rect.right
-    shr eax, 1          ; Centro de la ventana
-    mov xPos, eax
-    
-    ; Necesitamos recuperar el valor de espacio/2
-    mov ebx, 150        ; Espacio entre los dos grupos de elementos
-    shr ebx, 1          ; ebx = espacio/2
+    ; Posición para el texto del contador (icono + espacio)
+    mov eax, iconSize      ; Cargamos el valor de iconSize en eax
+    add xPos, eax          ; Sumamos eax a xPos
+    mov ebx, IconTextSpace
     add xPos, ebx
-    
+    invoke TextOut, hdc, xPos, centerYTexts, addr FlagCountStr, 2
+
+    ;-------------------------------------------------------------
+    ; MOSTRAR GRUPO CLOCK
+    ;-------------------------------------------------------------
+
+    ; Posición para el icono de reloj (contador + espacio150)
+    mov eax, textWidth      ; Cargamos el valor de iconSize en eax
+    add xPos, eax          ; Sumamos eax a xPos
+    mov ebx, GroupSpace
+    add xPos, ebx
+    mov ebx, IconTextSpace
+    sub xPos, ebx            ; Restamos 10 porque ya sumamos 10 después del icono de bandera
+
     ; Cargar el ícono de reloj
     invoke LoadImage, hInstance, IDI_CLOCK, IMAGE_ICON, iconSize, iconSize, LR_DEFAULTCOLOR
     mov hClockIcon, eax
     
-    ; Dibujar el ícono de reloj (en la misma altura que el de bandera)
-    invoke DrawIconEx, hdc, xPos, centerY, hClockIcon, iconSize, iconSize, 0, NULL, DI_NORMAL
-    
-    ; Obtener dimensiones del texto del timer para centrarlo verticalmente
-    invoke GetTextExtentPoint32, hdc, addr TimerStr, 5, addr textWidth
-    mov eax, textWidth
-    mov textWidth, eax
-    
-    ; Dibujar el timer (alineado verticalmente con el icono)
-    mov eax, iconSize
-    add xPos, eax        ; Usar registro para sumar iconSize
-    add xPos, 10        ; Pequeño espacio entre icono y texto
-    invoke TextOut, hdc, xPos, centerY, addr TimerStr, 5
+    ; Dibujar el ícono de reloj
+    invoke DrawIconEx, hdc, xPos, centerYIcons, hClockIcon, iconSize, iconSize, 0, NULL, DI_NORMAL
+
+    ; Posición para el texto del timer (reloj + espacio)
+    mov eax, iconSize      ; Cargamos el valor de iconSize en eax
+    add xPos, eax          ; Sumamos eax a xPos
+    mov ebx, IconTextSpace
+    add xPos, ebx
+    invoke TextOut, hdc, xPos, centerYTexts, addr TimerStr, 5
     
     ; Restaurar la fuente original y el modo de fondo
     invoke SelectObject, hdc, oldFont
